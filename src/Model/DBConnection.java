295022e9021588285dea7d25ac6fc4dbdae4d8f3
Pranjal Sharma <128293264+PranjalSharma007@users.noreply.github.com>
2025-06-01 13:12:21 +0530

    Update DBConnection.java

--- src/Model/DBConnection.java
+++ src/Model/DBConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:postgresql://pg.pg4e.com:5432/pg4e_7cfc5c1054?sslmode=require";
-    private static final String USER = "pg4e_7cfc5c1054";
-    private static final String PASSWORD = "pg4e_p_c788a74f96af495"; // from your .pgpass
+    private static final String URL = "jdbc:postgresql://localhost:5432/resumescanner";
+    private static final String USER = "EnterYourUsername";
+    private static final String PASSWORD = "EnterThePassword"; // from your .pgpass
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
